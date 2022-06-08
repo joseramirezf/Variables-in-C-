48c146eba348eccf07bcf536998bf2be8a0da3b3
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-08 13:42:44 -0600

    My first variables

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Variables");
+
+// int
+int age = 21;
+Console.WriteLine(age);
+
+int roomNumber;
+roomNumber = 4;
+Console.WriteLine(roomNumber);
+
+double pi = 3.14D;
+Console.WriteLine(pi);
+
+//char
+char firstLetter = 'A';
+Console.WriteLine(firstLetter);
+
+// bool
+bool accepted = false;
+Console.WriteLine(accepted);
+
+bool complete = true;
+Console.WriteLine(complete);
+
+// string
+string lastName = "Ramirez";
+Console.WriteLine(lastName);
+
+//Programa de lectura y escritura
+string fullName;
+Console.WriteLine("Ingrese su nombre completo");
+fullName = Console.ReadLine();
+
+Console.Write("Su nombre es:");
+Console.Write(fullName);
+
+///----
+Console.Write("\n¿En qué país vive usted?");
+string country = Console.ReadLine();
+
+Console.WriteLine("Su pais es:");
+Console.Write(country);
